d0b265a46a69834f9b74c9f523577de808ccd5a5
Alek <92643775+barealek@users.noreply.github.com>
2023-10-10 21:24:36 +0200

    Update client.go

--- cs2hop/internal/client.go
+++ cs2hop/internal/client.go
@@ -7,3 +7,25 @@ type Client struct {
 	Address uintptr
 	Offsets *Offsets
 }
+
+// Needs work
+func GetClientFrom(process *gomem.Process, offsets *Offsets) (*Client, error) {
+	address, err := process.GetModule("client.dll")
+	if err != nil {
+		return nil, err
+	}
+
+	client := &Client{Process: process, Address: address, Offsets: offsets}
+	return client, nil
+}
+
+func (c *Client) GetLocalPlayer() (uintptr, error) {
+	ptr, _ := c.Process.ReadUInt32(c.Address + uintptr(c.Offsets.ClientDll.DwLocalPlayer))
+	return (uintptr)(ptr), nil
+
+}
+
+func (c *Client) ForceJump() error {
+	ptr, _ := c.Process.ReadUInt32(c.Address + uintptr(c.Offsets.ClientDll.DwForceJump))
+	return c.Process.WriteByte(uintptr(ptr), 0x6)
+}
