2c04f441b3fc455f44b4e0294309949ff831842b
Alek <92643775+barealek@users.noreply.github.com>
2023-10-12 12:57:06 +0200

    Update client.go

--- cs2hop/internal/client.go
+++ cs2hop/internal/client.go
@@ -13,7 +13,14 @@ type Client struct {
 	Offsets *Offsets
 }
 
-func GetClientFromProcess(process *gomem.Process, offsets *Offsets) (*Client, error) {
+func GetClientFromProcessName(processName string, offsets *Offsets) (*Client, error) {
+	process, err := gomem.GetProcessFromName(processName)
+	if err != nil {
+		return nil, errors.New("failed to get process: " + processName)
+	}
+
+	process.Open()
+
 	address, err := process.GetModule("client.dll")
 	if err != nil {
 		return nil, err
@@ -24,6 +31,7 @@ func GetClientFromProcess(process *gomem.Process, offsets *Offsets) (*Client, er
 }
 
 func (c *Client) GetLocalPlayer() (uintptr, error) {
+
 	ptr, err := c.Process.ReadUInt32(c.Address + uintptr(c.Offsets.ClientDll.DwLocalPlayerPawn))
 	if err != nil {
 		return 0, errors.New("failed to read localplayer: " + err.Error())
@@ -32,13 +40,29 @@ func (c *Client) GetLocalPlayer() (uintptr, error) {
 }
 
 func (c *Client) PlayerIsInAir() (bool, error) {
-	pawn, err := c.GetLocalPlayer()
+	lp, err := c.GetLocalPlayer()
 	if err != nil {
 		return false, err
 	}
-	fmt.Println("Pawn address: ", pawn)
 
-	// ...
+	flags, err := c.Process.ReadByte(lp + uintptr(968))
+	if err != nil {
+		fmt.Println("FEjl")
+		return false, err
+	}
+	fmt.Println("Flags: ", flags)
 
 	return false, nil
+}
+
+func (c *Client) ForceJump() error {
+	lp, err := c.GetLocalPlayer()
+	if err != nil {
+		return err
+	}
+
+	flags, err := c.Process.ReadByte(lp + uintptr(c.Offsets.ClientDll.DwForceJump))
+	fmt.Println("Flags: ", flags)
+	// ...
+	return err
 }
